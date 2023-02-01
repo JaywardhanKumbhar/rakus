1192e7965b4e52df4747a8855c1c7093d7d1989d
Jaywardhan Kumbhar <jaywardhankumbhar203@gmail.com>
2023-02-01 16:37:48 +0530

    first

---
+++ Controllers/HRController/HRController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore;
+using System.Collections.Generic;
+using MyWebApi.Models;
+using MyWebApi.DAL;
+namespace MyWebApi.controllers;
+//http:\\localhost:5080\hr
+[ApiController]
+[Route("[controller]")]
+public class HRController : ControllerBase
+{
+  private readonly ILogger<HRController> _logger;
+  public HRController(ILogger<HRController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetAllEmployees")]
+    public IEnumerable<Employee> Get()
+    {
+        List<Employee> employee=DBManager.GetAll();
+        return employee.ToArray();
+    }
+
+    
+    [HttpGet]
+    [Route("{id}")]
+    public ActionResult<Employee>  GetById(int id){
+       Employee emp=DBManager.GetById(id);
+       return emp;
+    }
+    [HttpDelete]
+    [Route("{id}")]
+    public ActionResult<Employee>  DeleteById(int id){
+       Employee emp=DBManager.GetById(id);
+       bool status=DBManager.DeleteById(id);
+       if(status){
+            return NoContent();
+       }   
+       else{
+        return NotFound();
+
+       }
+       return emp;
+    }
+
+    [HttpPut("{id}")]
+    public  ActionResult Update(int id, Employee emp)
+    {
+        bool status=DBManager.Update(id, emp);
+       if(status)
+       {
+            return NoContent();
+       }   
+       else
+       {
+        return NotFound();
+       }
+    }
+
+
+    [HttpPost]
+    public  ActionResult Insert( Employee emp)
+    {
+        bool status=DBManager.Insert(emp);
+       if(status)
+       {
+            return NoContent();
+       }   
+       else
+       {
+        return NotFound();
+       }
+    }
+}
---
+++ DAL/DBManager/DBManager.cs
@@ -0,0 +1,42 @@
+﻿namespace MyWebApi.DAL;
+using  MyWebApi.Models;
+public class DBManager
+{
+    public static List<Employee>GetAll()
+    {
+        List<Employee>employees=new List<Employee>();
+        employees.Add(new Employee{Id=1, FirstName="Manav", LastName="Kon", Email="mk@gmail.com",ContactNumber="9834324505"});
+       employees.Add(new Employee{Id=2, FirstName="Rakus", LastName="Lanka", Email="rl@gmail.com",ContactNumber="9656546162"});
+       employees.Add(new Employee{Id=3, FirstName="Lakhan", LastName="Pandey", Email="lp@gmail.com",ContactNumber="9897969594"});
+       return employees;
+    }
+
+    public static Employee GetById(int id)
+    {
+        Employee emp=new Employee{Id=id, FirstName="Manav",
+                    LastName="Kon", Email="mk@gmail.com",
+                    ContactNumber="9834324505"};
+        return emp;
+    }
+
+    public static bool DeleteById(int Id)
+    {
+        bool status=true;
+        return status;
+    }
+
+     public static bool Update(int Id,Employee emp)
+    {
+        bool status=true;
+        return status;
+    }
+
+     public static bool Insert(Employee emp)
+    {
+        bool status=true;
+        Console.WriteLine(emp.FirstName+" "+emp.LastName);
+        return status;
+    }
+    
+    
+}
